f9c1a4de75b2130f3eb1e403d4f3eb4488674763
Kulinich Anton <kulinich.anton@airslate.com>
2023-04-17 17:26:15 +0300

    DI container resolves classes

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ return [
     'containers' => [
         \Psr\Log\LoggerInterface::class => [
             'class' => \Monolog\Logger::class,
-            'name' => 'mono',
+            'name' => 'log',
             'handlers' => [
                 '@file_log_handler',
                 '@console_log_handler',
@@ -12,7 +12,7 @@ return [
         ],
         'file_log_handler' => [
             'class' => \Monolog\Handler\StreamHandler::class,
-            'stream' => __DIR__ . '/../storage/log.txt'
+            'stream' => fn (\DateTime $time) => __DIR__ . '/../storage/' . $time->format('Y-m-d') . '.log',
         ],
         'console_log_handler' => [
             'class' => \Monolog\Handler\StreamHandler::class,
--- src/Foundation/DI/ServiceFactory.php
+++ src/Foundation/DI/ServiceFactory.php
@@ -16,7 +16,7 @@ class ServiceFactory
             $class = $description->className();
             $params = $description->params();
             $params = $this->resolveParams($params);
-            return $params ? new $class(...$params) : new $class();
+            return $this->buildClassVal($class, $params);
         } catch (\Throwable $e) {
             $msg = "Can't resolve '{$description->className()}' dependency." . PHP_EOL;
             $msg .= $e->getMessage();
@@ -27,7 +27,7 @@ class ServiceFactory
     private function resolveParams(array $params): array
     {
         foreach ($params as $key => $val) {
-            $val = $this->value($val);
+            $val = $this->buildVal($val);
             if ($this->container->has($key)) {
                 $val = $this->container->get($key);
             }
@@ -37,17 +37,64 @@ class ServiceFactory
         return $params;
     }
 
-    private function value($value, ...$args)
+    private function buildVal($value)
     {
         if (is_array($value)) {
-            foreach ($value as $idx => $item) {
-                $value[$idx] = $this->value($item);
-            }
+            $value = $this->buildArrayVal($value);
         }
         if (is_string($value) && str_starts_with($value, '@')) {
             $value = substr($value, 1, strlen($value));
             return $this->container->get($value);
         }
-        return $value instanceof \Closure ? $value(...$args) : $value;
+
+        return $value instanceof \Closure ? $this->buildClosureVal($value) : $value;
+    }
+
+    private function buildArrayVal(array $value): array
+    {
+        foreach ($value as $idx => $item) {
+            $value[$idx] = $this->buildVal($item);
+        }
+        return $value;
+    }
+
+    private function buildClosureVal(callable $closure)
+    {
+        $reflection = new \ReflectionFunction($closure);
+        $reflectedArguments  = $reflection->getParameters();
+        $resolvedArguments = [];
+        foreach ($reflectedArguments as $argument) {
+            $type = $argument->getType();
+            if ($type->isBuiltin()) {
+                throw new WrongConfigurationException("Can't inject builtin type");
+            }
+            $description = new ServiceDescription(['class' => $type->getName()]);
+            $resolvedArguments[] = $this->build($description);
+        }
+
+        return $closure(...$resolvedArguments);
+    }
+
+    public function buildClassVal(string $class, array $params = []): mixed
+    {
+        $reflection = new \ReflectionClass($class);
+        $constructor = $reflection->getConstructor();
+        if (empty($constructor)) {
+            return new $class();
+        }
+        $constructorParams = $constructor->getParameters();
+        foreach ($constructorParams as $p) {
+            $pType = $p->getType();
+            $pName = $p->getName();
+            if (!$pType instanceof \ReflectionNamedType) {
+                continue;
+            }
+            if (isset($params[$pName]) || $pType->isBuiltin() || $p->isOptional()) {
+                continue;
+            }
+            $params[$pName] = $this->buildClassVal($pType->getName());
+        }
+
+        return new $class(...$params);
     }
 }
